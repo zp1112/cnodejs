97027128f8917320725de6296c53d232d4dbb2e3
Candy <candy@sh.gg>
2016-10-14 16:27:13 +0800

    update .gitignore

--- js/main.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  if(!localStorage.getItem('userInfo')){
-    window.location = 'login.html';
-  }
-  $('#logout').on('click', function(){
-    localStorage.removeItem('userInfo');
-    window.location = 'login.html';
-  })
-})()
